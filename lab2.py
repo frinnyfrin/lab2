a367862fdb743641482f28f04dd988d767551da4
Frinze <frinzetolentino.23@ichat.sp.edu.sg>
2024-04-25 23:37:37 +0800

    lolool

--- lab2.py
+++ lab2.py
@@ -1,4 +1,48 @@
-print ("ET0735 (DevOps for AIoT) -  Lab 2 - Introduction to Python")
-def funcName (parameter1, parameter2):
-    print ("this is a dummy function")
-    return 10
+import statistics
+
+def main():
+    display_main_menu()
+    lst = get_user_input()
+    calc_average(lst)
+    calc_minmax_temperature(lst)
+    find_median(lst)
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (eg. 1,2,3,4)")
+
+def get_user_input():
+    user = input("Enter a list: ").split(",")
+    print (user)
+    user = [float(i) for i in user]
+    print (user)
+    return user    
+
+def calc_average(lst):
+    total=sum(lst)
+    avg_temp=total/len(lst)
+    print("average temperature is:"+str(avg_temp))
+    return avg_temp 
+def calc_minmax_temperature(num_list):
+    max_number= max(num_list)
+    min_number = min(num_list)
+    mmlist=[min_number,max_number]
+    print("Min Temp, Max Temp"+str(mmlist))
+    return mmlist
+
+def calc_ascending(lst):
+    print("sort temp in ascending order")
+    lst.sort()
+    print(lst)
+    return lst
+
+def find_median(lst):
+    lol = calc_ascending(lst)
+    median = statistics.median(lol)
+    print("the median is " +str(median))
+    return median
+    
+
+         
+
+if __name__ == "__main__":
+    main()
