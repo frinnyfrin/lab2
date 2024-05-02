c08e3ed6f30b861e20a2992fba8d9714280df94a
Frinze <frinzetolentino.23@ichat.sp.edu.sg>
2024-05-02 15:20:29 +0800

    lol

--- bmi.py
+++ bmi.py
@@ -6,12 +6,15 @@ def calculate_bmi(height, weight):
     if bmi<18.5:
         underweight=-1
         print("You are underweight")
+        return -1
     if 18.5<=bmi<=25.0:
         normal_weight=0
         print("You are normal weight")
+        return 0
     if bmi>25.0:
         overweight = 1
         print("you are overweight")
+        return 1
 calculate_bmi(weight=57,height=1.73)
 
 
